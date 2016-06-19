ef56fb5ff3c79168a9164ff57898053fdb227ddd
whisky <illuminati.w@gmail.com>
2016-06-19 15:34:10 +0800

    first test
    add a test filr

--- scraper.py
+++ scraper.py
@@ -22,3 +22,9 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+import scraperwiki
+import lxml.html
+
+html = scraperwiki.scrape("http://npo.moi.gov.tw/npom/homepage/detail/268881482")
+print(html)
